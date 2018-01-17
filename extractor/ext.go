a2ca8159b88cb5e0dd1c44ea780be9b69c39e3d9
Hwi Ahn <byron1st@icloud.com>
2018-01-17 21:25:42 +0900

    build skeleton code for /dir

--- extractor/ext.go
+++ extractor/ext.go
@@ -1,1 +1,25 @@
 package extractor
+
+type Package struct {
+	Id          string
+	Name        string
+	PackagePath string
+	PackageName string
+	PackageDir  string
+	IsExternal  bool
+	IsStd       bool
+}
+
+type Dep struct {
+	Id    string
+	From  string
+	To    string
+	Count int
+}
+
+func GetDirTree(rootPath string) (error, []*Package, []*Dep) {
+	packageList := make([]*Package, 0)
+	depList := make([]*Dep, 0)
+
+	return nil, packageList, depList
+}
--- main.go
+++ main.go
@@ -3,16 +3,43 @@ package main
 import (
 	"net/http"
 	"log"
+	"encoding/json"
+	"github.com/byron1st/godepexplorer/extractor"
 )
+
+type dirReqStruct struct {
+	RootPath string
+}
+
+type dirResStruct struct {
+	Nodes []*extractor.Package `json:"nodes"`
+	Edges []*extractor.Dep     `json:"edges"`
+}
+
+func getDir(writer http.ResponseWriter, request *http.Request) {
+	var req dirReqStruct
+	err := json.NewDecoder(request.Body).Decode(&req)
+
+	if err != nil {
+		http.Error(writer, err.Error(), 400)
+	}
+
+	err, nodes, edges := extractor.GetDirTree(req.RootPath)
+	json.NewEncoder(writer).Encode(&dirResStruct{nodes, edges})
+}
 
 func main() {
 	http.Handle("/explorer/", http.FileServer(http.Dir(".")))
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		if request.URL.Path == "/" {
-			http.ServeFile(writer, request, "explorer/index.html")
-			return
+
+	/**
+	GET /dir 해당 dir 내의 서브 dir를 계층구조로 반환
+		response: {
+			nodes: [] // dir name list
+			edges: [] // composition relation
 		}
-	})
+ 	GET /dep?nodeId={}
+	 */
+	http.HandleFunc("/dir", getDir)
 
 	if err := http.ListenAndServe("localhost:1111", nil); err != nil {
 		log.Fatal(err)
