6e99609492a23dfc07bdb2b183e780dd3859bd75
Hwi Ahn <byron1st@icloud.com>
2018-01-20 21:28:08 +0900

    separate server from main

--- main.go
+++ main.go
@@ -1,46 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/byron1st/godepexplorer/extractor"
-)
-
-type reqStruct struct {
-	PkgName string `json:"pkgName"`
-}
-
-type resStruct struct {
-	Nodes []*extractor.Package `json:"nodes"`
-	Edges []*extractor.Dep     `json:"edges"`
-}
-
-func handlerGenerator(extractorFunc func(string) ([]*extractor.Package, []*extractor.Dep, error)) func(http.ResponseWriter, *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		var req reqStruct
-		err := json.NewDecoder(request.Body).Decode(&req)
-
-		if err != nil {
-			http.Error(writer, err.Error(), 400)
-		}
-
-		pkgName := req.PkgName
-		fmt.Printf("Package name: %s\n", pkgName)
-		nodes, edges, err := extractorFunc(pkgName)
-
-		fmt.Printf("nodes len: %d, edges len: %d\n", len(nodes), len(edges))
-		json.NewEncoder(writer).Encode(&resStruct{nodes, edges})
-	}
-}
+import "github.com/byron1st/godepexplorer/server"
 
 func main() {
-	http.HandleFunc("/dir", handlerGenerator(extractor.GetDirTree))
-	http.HandleFunc("/dep", handlerGenerator(extractor.GetDeps))
-
-	if err := http.ListenAndServe("localhost:1111", nil); err != nil {
-		log.Fatal(err)
-	}
+	s := server.MakeServer("localhost", 1111)
+	s.StartServer()
 }
---
+++ server/server.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/byron1st/godepexplorer/extractor"
+)
+
+type Server struct {
+	host string
+	port int
+}
+
+type reqStruct struct {
+	PkgName string `json:"pkgName"`
+}
+
+type resStruct struct {
+	Nodes []*extractor.Package `json:"nodes"`
+	Edges []*extractor.Dep     `json:"edges"`
+}
+
+func MakeServer(host string, port int) *Server {
+	setRoute()
+
+	server := &Server{host, port}
+	// TODO: check the validity of host and port.
+	return server
+}
+
+func (server *Server) StartServer() {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", server.host, server.port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setRoute() {
+	http.HandleFunc("/dir", handlerGenerator(extractor.GetDirTree))
+	http.HandleFunc("/dep", handlerGenerator(extractor.GetDeps))
+}
+
+func handlerGenerator(extractorFunc func(string) ([]*extractor.Package, []*extractor.Dep, error)) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		var req reqStruct
+		err := json.NewDecoder(request.Body).Decode(&req)
+
+		if err != nil {
+			http.Error(writer, err.Error(), 400)
+		}
+
+		pkgName := req.PkgName
+		fmt.Printf("Package name: %s\n", pkgName)
+		nodes, edges, err := extractorFunc(pkgName)
+
+		fmt.Printf("nodes len: %d, edges len: %d\n", len(nodes), len(edges))
+		json.NewEncoder(writer).Encode(&resStruct{nodes, edges})
+	}
+}
