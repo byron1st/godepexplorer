14083e6a4252ff85a8918e952c90fc4f26fe7c41
Hwi Ahn <byron1st@icloud.com>
2018-01-20 21:10:36 +0900

    move error to the last return argument

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"net/http"
-	"log"
 	"encoding/json"
-	"github.com/byron1st/godepexplorer/extractor"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/byron1st/godepexplorer/extractor"
 )
 
 type reqStruct struct {
@@ -17,7 +18,7 @@ type resStruct struct {
 	Edges []*extractor.Dep     `json:"edges"`
 }
 
-func handlerGenerator(extractorFunc func(string) (error, []*extractor.Package, []*extractor.Dep)) func(http.ResponseWriter, *http.Request) {
+func handlerGenerator(extractorFunc func(string) ([]*extractor.Package, []*extractor.Dep, error)) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		var req reqStruct
 		err := json.NewDecoder(request.Body).Decode(&req)
@@ -28,7 +29,7 @@ func handlerGenerator(extractorFunc func(string) (error, []*extractor.Package, [
 
 		pkgName := req.PkgName
 		fmt.Printf("Package name: %s\n", pkgName)
-		err, nodes, edges := extractorFunc(pkgName)
+		nodes, edges, err := extractorFunc(pkgName)
 
 		fmt.Printf("nodes len: %d, edges len: %d\n", len(nodes), len(edges))
 		json.NewEncoder(writer).Encode(&resStruct{nodes, edges})
@@ -36,16 +37,6 @@ func handlerGenerator(extractorFunc func(string) (error, []*extractor.Package, [
 }
 
 func main() {
-	http.Handle("/explorer/", http.FileServer(http.Dir(".")))
-
-	/**
-	POST /dir 해당 dir 내의 서브 dir를 계층구조로 반환. POST data 는 { rootPkgName: "" } 으로 받음.
-		response: {
-			nodes: [] // dir name list
-			edges: [] // composition relation
-		}
- 	POST /dep
-	 */
 	http.HandleFunc("/dir", handlerGenerator(extractor.GetDirTree))
 	http.HandleFunc("/dep", handlerGenerator(extractor.GetDeps))
 
